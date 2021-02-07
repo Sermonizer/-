5dd8b9cccfae08e34a7ad47848d9afe115427cc0
tengxu <1025753441@qq.com>
2021-02-07 22:21:22 +0800

    homepage

--- src/component/Home.vue
+++ src/component/Home.vue
@@ -1,7 +1,21 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">登出</el-button>
-  </div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <img src="../assets/jd.jpg" alt="系统图标">
+        <span>后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">登出</el-button>
+    </el-header>
+    <!-- 主体区域 -->
+    <el-container>
+      <!-- 侧边栏区域 -->
+      <el-aside width="200px">Aside</el-aside>
+      <!-- 右侧内容区域 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -19,4 +33,38 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.home-container {
+  width: 100%;
+  height: 100%;
+}
+
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  // 左右对齐
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+    img {
+      height: 60px;
+      width: 60px;
+    }
+  }
+}
+
+.el-aside {
+  background-color: #333744;
+}
+
+.el-main {
+  background-color: #EAEDF1;
+}
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 // 按需加载element-ui的组件
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import {
+  Button,
+  Form,
+  FormItem,
+  Input,
+  Message,
+  Container,
+  Header,
+  Aside,
+  Main
+} from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 // 将引入的组件注册为全局组件
@@ -9,3 +19,8 @@ Vue.use(Form)
 Vue.use(Input)
 Vue.use(FormItem)
 Vue.prototype.$message = Message
+
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
